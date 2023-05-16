e7f9636e4d90e4a563f115ddaf03ea5c77369129
Projckt <projckt@protonmail.com>
2023-05-16 12:44:21 +0530

    adds and gets gateways

--- src/components/gateway/dals/index.ts
+++ src/components/gateway/dals/index.ts
@@ -1,5 +1,6 @@
 // Read
 export { dal_Gateway_Read } from "./read/dal_Gateway_Read";
+export { dal_Gateway_Read_Backend } from "./read/dal_Gateway_Read_Backend";
 
 // Write
 export { dal_Gateway_Write } from "./write/dal_Gateway_Write";
