76bdfd0e9587eab43399824a2375eda73652f60e
Projckt <projckt@protonmail.com>
2023-06-08 00:38:16 +0530

    fixes build script

--- build.sh
+++ build.sh
@@ -25,7 +25,7 @@ git commit -m "Deploy build `date`"
 git remote add origin git@github.com-slim-dl:slim-dl/app-api-prod.git
 
 # Push codebase
-git push -u origin main
+git push -f origin main
 
 # Restore
 cd ..
