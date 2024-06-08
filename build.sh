d45a8a135809cb273e29ebe7b6af08021d28d81f
Projckt <projckt@protonmail.com>
2024-06-08 17:05:48 +0530

    fixes

--- build.sh
+++ build.sh
@@ -1,27 +1,20 @@
-# Delete previous /prod
-cd ..
-rm -rf prod
+## Delete old /prod
+rm -rf ../prod
 
-# Build the codebase
-cd dev
+## Build codebase
 npm run build
 
-# Move /prod out of /dev
+# Init prod
 mv prod ../
-
-# Copy configs to /prod
 cp {.env,.env-bak,.gitignore,package.json} ../prod
-
-# Install npm dependencies in /prod
-cd ..
-cd prod
-npm install
+npm --prefix ../prod install
 
 # Initialize git in /prod
-git init
-git add --all
-git commit -m "Prod build - `date`"
-git remote add origin git@github.com-projckt:projckt/starter_webapp-api-prod.git
+git init ../prod
+# git -C ../prod remote add origin git@github.com-{username}:{username}/{repo_name}__prod.git
+git -C ../prod remote add origin git@github.com-projckt:projckt/starter_webapp-api-prod.git
 
 # Push codebase to remote
-git push -f origin main
+git -C ../prod add --all
+git -C ../prod commit -m "Deploy build `date`"
+git -C ../prod push origin main --force
