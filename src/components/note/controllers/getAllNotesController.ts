68b47f13133309f6c06806f91ad77105a390e5ce
Projckt <projckt@protonmail.com>
2024-06-22 19:53:16 +0530

    gets all notes

--- src/components/note/controllers/getAllNotesController.ts
+++ src/components/note/controllers/getAllNotesController.ts
@@ -1,5 +1,20 @@
 import { Request, Response } from "express";
+import { readAllNotesByAccountId } from "../dals";
+import { Var } from "../../../global/var";
 
 export const getAllNotesController = async (req: Request, res: Response) => {
-  return res.send("Get all notes controller");
+  let notes: any = await readAllNotesByAccountId(res.locals.accountId);
+  if (!notes) {
+    console.log(`${Var.app.emoji.failure} Could not fetch notes`);
+    return res.status(400).json({
+      success: false,
+      message: `${Var.app.emoji.failure} Could not fetch notes`,
+    });
+  }
+
+  return res.status(200).json({
+    success: true,
+    message: `${Var.app.emoji.success} Notes fetched`,
+    payload: notes,
+  });
 };
